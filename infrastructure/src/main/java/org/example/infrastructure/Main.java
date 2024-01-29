79624688b86964c747f5049e68fa2169de1fda80
Ar3secchim <renarasecchim@gmail.com>
2024-01-29 11:34:55 -0300

    update: update config application.properties

--- infrastructure/src/main/java/org/example/infrastructure/Main.java
+++ infrastructure/src/main/java/org/example/infrastructure/Main.java
@@ -1,9 +1,15 @@
 package org.example.infrastructure;
 
-import org.example.application.UseCase;
+import org.example.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+@SpringBootApplication
 public class Main {
+
   public static void main(String[] args) {
-    System.out.println(new UseCase().execute());
+    System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+    SpringApplication.run(WebServerConfig.class, args);
   }
 }
