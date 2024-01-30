2fdd2e581a096080065b78abaae906aab78146c5
Ar3secchim <renarasecchim@gmail.com>
2024-01-30 15:50:51 -0300

    test: create for case test validation task not name null

--- domain/src/test/java/org/example/domain/TaskTest.java
+++ domain/src/test/java/org/example/domain/TaskTest.java
@@ -1,6 +1,8 @@
 package org.example.domain;
 
+import org.example.domain.exceptions.DomainException;
 import org.example.domain.task.Task;
+import org.example.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +25,22 @@ public class TaskTest {
     Assertions.assertNotNull(actualTasks.getCreatedTime());
     Assertions.assertNotNull(actualTasks.getUpdateTime());
     Assertions.assertNull(actualTasks.getDeleteTime());
+  }
+
+  @Test
+  public void givenAnInvalidNameNull_whenCallNewTaskAndValidate_thenShouldException(){
+    final var expectedErrorCount = 1;
+    final var expectedErrorMessage = "'name' should be null";
+    final var expectedDescription = "false";
+    final var expectedValue = "10";
+
+    final var actualTasks =
+      Task.newTask(null ,expectedDescription, expectedValue);
+
+    final var actualException =
+      Assertions.assertThrows(DomainException.class, () -> actualTasks.validate(new ThrowsValidationHandler()));
+
+    Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
   }
 }
